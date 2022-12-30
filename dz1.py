cef811de814679517f78909d047dc9d0a979a5b8
Kashira2 <nekit.egorov@icloud.com>
2022-12-30 06:41:59 +0300

    file1

--- dz1.py
+++ dz1.py
@@ -6,11 +6,13 @@ my_list = []
 
 def creat_list(my_list, num1):
     for i in range(1, num1+2):
-        kof = int(random.uniform(0, 100))
-        if kof > 1:
+        kof = int(random.uniform(-100, 100))
+        if kof > 1 or kof < 0:
             my_list.append(str(kof))
         elif kof == 1:
-            my_list.append('c') 
+            my_list.append('c')
+        elif kof == -1:
+            my_list.append('-c') 
         else:
             my_list.append('z') 
 
@@ -19,6 +21,7 @@ def creat_list(my_list, num1):
     my_string = my_string.split(' ') 
 
     return my_string
+print(creat_list(my_list, num1))
 
 def string_end(num1, my_string):
     new_string = ''
@@ -34,9 +37,9 @@ def string_end(num1, my_string):
     return new_string
 
 print(string_end(num1, creat_list(my_list, num1)).replace('*x**0', '').replace('x**0', '1')\
-    .rstrip().replace(' ', ' + ').replace('**1', '') + ' = 0')
+    .rstrip().replace(' ', ' + ').replace('**1', '').replace(' + -', ' - ') + ' = 0')
 
 f = open('file.txt', 'w')
 f.write(string_end(num1, creat_list(my_list, num1)).replace('*x**0', '').replace('x**0', '1')\
-    .rstrip().replace(' ', ' + ').replace('**1', '') + ' = 0')
+    .rstrip().replace(' ', ' + ').replace('**1', '').replace(' + -', ' - ') + ' = 0')
 f.close
